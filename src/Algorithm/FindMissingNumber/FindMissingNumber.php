7f10976e8f4665627fb1307d4db5d396edb269f9
tiger-seo <tiger.seo@gmail.com>
2013-08-05 18:17:14 +0300

    test for find missing number algorithm

---
+++ src/Algorithm/FindMissingNumber/FindMissingNumber.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author tiger
+ */
+
+namespace Algorithm\FindMissingNumber;
+
+class FindMissingNumber
+{
+    /**
+     * @param array $ary
+     * @return int
+     */
+    public function find(array $ary)
+    {
+    }
+}
