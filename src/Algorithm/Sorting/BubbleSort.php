00c21f20f7694e5facddbff79e9d6d41672a2264
tiger-seo <tiger.seo@gmail.com>
2013-08-12 23:43:53 +0300

    bubble sort algorithm implemented

--- src/Algorithm/Sorting/BubbleSort.php
+++ src/Algorithm/Sorting/BubbleSort.php
@@ -9,6 +9,26 @@ class BubbleSort
 {
     public function sort(array $ary)
     {
-        return [];
+        if (count($ary) < 2) {
+            return $ary;
+        }
+
+        $j = count($ary);
+        do {
+            $i = 1;
+            $swappingOccurred = false;
+            while ($i < $j) {
+                if ($ary[$i - 1] > $ary[$i]) {
+                    $temp             = $ary[$i - 1];
+                    $ary[$i - 1]      = $ary[$i];
+                    $ary[$i]          = $temp;
+                    $swappingOccurred = true;
+                }
+                $i++;
+            }
+            $j--;
+        } while ($swappingOccurred);
+
+        return $ary;
     }
 }
